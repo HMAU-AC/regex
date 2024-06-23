9c6096fdff54239d937c51de2b5d7af1a95cc6a1
zhou yu <33508419+HMAU-AC@users.noreply.github.com>
2024-06-23 19:32:40 +0800

    比较笨，继续修复

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,43 @@
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/manifest.json',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('my-cache').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                // '/styles.css',
-                '/app.js',
-                '/icon-192x192.png',
-                '/icon-512x512.png'
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
 });
