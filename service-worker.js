cc4631d59b8511853e3f34eeb9a87aca9bee8af4
zhou yu <33508419+HMAU-AC@users.noreply.github.com>
2024-06-25 12:24:32 +0800

    尝试

--- service-worker.js
+++ service-worker.js
@@ -3,14 +3,21 @@ const urlsToCache = [
   '/regex/',
   '/regex/index.html',
   '/regex/regex.html',
+  '/regex/sc.html',
+  '/regex/yh.html',
+  '/regex/st.html',
   '/regex/styles.css',
   '/regex/app.js',
   '/regex/theme-color.js',
   '/regex/manifest.json',
-  '/regex/icon-192x192.png',
-  '/regex/icon-512x512.png'
+  '/regex/icons/google.png',
+  '/regex/icons/bing.png',
+  '/regex/icons/baidu.png',
+  '/regex/icons/icon-192x192.png',
+  '/regex/icons/icon-512x512.png'
 ];
 
+// 安装事件
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -21,11 +28,32 @@ self.addEventListener('install', event => {
   );
 });
 
+// 获取事件
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
         if (response) {
+          // 检查缓存的响应是否过期
+          const headers = response.headers;
+          const cacheControl = headers.get('Cache-Control');
+          const expires = headers.get('Expires');
+          
+          if (cacheControl || expires) {
+            const now = new Date().getTime();
+            const maxAge = cacheControl ? parseInt(cacheControl.split('=')[1]) * 1000 : null;
+            const expiresTime = expires ? new Date(expires).getTime() : null;
+
+            if ((maxAge && now - response.time > maxAge) || (expiresTime && now > expiresTime)) {
+              // 如果缓存过期，从网络获取新的响应
+              return fetch(event.request).then(networkResponse => {
+                caches.open(CACHE_NAME).then(cache => {
+                  cache.put(event.request, networkResponse.clone());
+                });
+                return networkResponse;
+              });
+            }
+          }
           return response;
         }
         return fetch(event.request);
@@ -33,6 +61,7 @@ self.addEventListener('fetch', event => {
   );
 });
 
+// 激活事件
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
